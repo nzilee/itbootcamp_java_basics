bf319c33124cc5d03f0bbf66c0042d3b7767fdae
Nikola Zivkovic <nzivkovic7@gmail.com>
2022-05-13 20:30:38 +0200

    Predavanja 13.05 Git Zadatak1

---
+++ osnove_jave/src/main/java/p_13_5_2022/Git_Zadatak1.java
@@ -0,0 +1,12 @@
+package p_13_5_2022;
+
+public class Git_Zadatak1 {
+    public static void main(String[] args) {
+        System.out.println("Sve komande: \n" +
+                "git clone \n" +
+                "git add \n" +
+                "git status \n" +
+                "git commit -m \n" +
+                "git push");
+    }
+}
