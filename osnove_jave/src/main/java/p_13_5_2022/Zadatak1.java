86446be917a5d43a4ba1fddd72bc36a9c7f7ac95
Nikola Zivkovic <nzivkovic7@gmail.com>
2022-05-13 19:59:34 +0200

    Minor typo fix

--- osnove_jave/src/main/java/p_13_5_2022/Zadatak1.java
+++ osnove_jave/src/main/java/p_13_5_2022/Zadatak1.java
@@ -2,6 +2,6 @@ package p_13_5_2022;
 
 public class Zadatak1 {
     public static void main(String[] args) {
-        System.out.println("Hello borld!");
+        System.out.println("Hello World!");
     }
 }
