ace7d70a304e6ed8de6bbdd6d7554139fc6cfec6
Nikola Zivkovic <nzivkovic7@gmail.com>
2022-05-16 12:19:47 +0200

    Domaci 13.05.2022. Zadatak 4

---
+++ osnove_jave/src/main/java/d_13_5_2022/Zadatak4.java
@@ -0,0 +1,31 @@
+package d_13_5_2022;
+
+import java.util.Scanner;
+
+public class Zadatak4 {
+    public static void main(String[] args) {
+//  Napisati program koji uci korisnika da kaze “Molim te”. Program od
+//  korisnika ucitava zahteve koje treba da izvrsi ali jedino reaguje
+//  ukoliko korisnik lepo zamoli ( u zahtevu unese “Molim te”).
+//  Ukoliko korisnik unese zahtev u vidu naredjivanja, program ne
+//  reaguje na takve zahteve tj. nista ne ispisuje, a ukoliko korisnik
+//  zamoli tada program ispisuje “Dogovoreno!”. Program radi dok
+//  korisnik ne unese “Hvala!”. Na kraju programa ispisati “Cao”
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Unesite zahtev: ");
+        String zahtev = s.nextLine();
+
+
+        while (zahtev.contains("molim te")) {
+            System.out.println("Dogovoreno!");
+
+            System.out.print("Unesite zahtev: ");
+            zahtev = s.nextLine();
+            if (zahtev.contains("hvala")){
+                System.out.println("Cao");
+            }
+        }
+    }
+}
